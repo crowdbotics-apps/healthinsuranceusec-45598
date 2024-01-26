0c32fa1e80a230f492f312cb94c8173c26961cdb
Crowdbotics <team@crowdbotics.com>
2024-01-26 20:08:04 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "healthinsuranceusec-45598",
+  url: "https://healthinsuranceusec-45598.botics.co",
+  api: "https://healthinsuranceusec-45598.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
